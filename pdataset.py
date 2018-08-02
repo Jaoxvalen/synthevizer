b17d25d1c57ac4bde0c4423600951c9a03f37e16
Joao <jaoxvalen@gmail.com>
2018-08-01 19:43:23 -0500

    generador listo

--- pdataset.py
+++ pdataset.py
@@ -1,6 +1,8 @@
 import random
 from noise import snoise3
 
+_MAX = 999999
+_MIN = -999999
 
 def get_noise(x, y, seed, freq):
 
@@ -11,10 +13,18 @@ def get_noise(x, y, seed, freq):
 
 class PDataset(object):
 
+    
+
     def __init__(self, pathfile):
 
         self.lats = []
         self.lons = []
+        self.temp = []
+        self.__reset_lu()
+        self.minLat = _MAX
+        self.maxLat = _MIN
+        self.minLon = _MAX
+        self.maxLon = _MIN
         self.__get_lat_log(pathfile)
 
     def __get_lat_log(self, pathfile):
@@ -24,25 +34,39 @@ class PDataset(object):
         for line in range(1, len(contents)):
             line_split = contents[line].split(' ')
             lat, lon = line_split[0], line_split[1][:-1]
-            self.lats.append(float(lat) )
-            self.lons.append(float(lon) )
+
+            val_lat = float(lat)
+            val_lon = float(lon)
+
+            self.lats.append( val_lat )
+            self.lons.append( val_lon )
+
+            self.minLat = min(val_lat, self.minLat)
+            self.maxLat = max(val_lat, self.maxLat)
+
+            self.minLon = min(val_lon, self.minLon)
+            self.maxLon = max(val_lon, self.maxLon)
         
         f.close()
+    
+    def __reset_lu(self):
+        self.lower = _MAX
+        self.upper = _MIN
 
-    def get_noise_values(self):
+    def gen_noise_values(self):
 
         seed = random.randint(1,255)
         freq = random.randint(10,30)
-        lower = 999999
-        upper = -999999
-        values = []
+        self.__reset_lu()
 
         for i in range(0, len( self.lats )):
             val = get_noise(self.lats[i], self.lons[i] , seed, freq)
-            values.append(val)
-            lower = min(val, lower)
-            upper = max(val, upper)
+            self.temp.append(val)
+            self.lower = min(val, self.lower)
+            self.upper = max(val, self.upper)
+
+
         
-        return lower, upper, values
+
 
 
--- pmap.py
+++ pmap.py
@@ -1,14 +1,22 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 
 
 class PMap(object):
 
-    def __init__(self, _data, _projection, _color_map, _llcrnrlat,
-                 _llcrnrlon, _urcrnrlat, _urcrnrlon, _lat_ts, _resolution,
-                 _n_quantized=0, _draw_coast_lines=True, _draw_countries=False, _draw_states=True):
+    def __init__(self, 
+                _data, _projection, 
+                _color_map, _llcrnrlat,
+                 _llcrnrlon, _urcrnrlat, _urcrnrlon, _lat_ts, _resolution, _lon_0=0, _lat_0=0,  _n_tick_lat=6, _n_tick_lon=8,
+                 _n_quantized=0, _draw_coast_lines=True, _draw_countries=False, _draw_states=True,
+                 _background='none',
+                 _ocean_color='white',
+                 _land_color='white',
+                 _color_map_pos='right', _color_map_cax=False, _color_map_shrink=0.5, _color_map_orientation='vertical', 
+                 _color_map_thickness='5%', _color_map_label='value'):
 
         self.projection = _projection
         self.llcrnrlat = _llcrnrlat
@@ -22,12 +30,41 @@ class PMap(object):
         self.draw_countries = _draw_countries
         self.draw_states = _draw_states
         self.data = _data
-        self.color_map = plt.cm.get_cmap(_color_map)
+        self.n_tick_lat = _n_tick_lat
+        self.n_tick_lon = _n_tick_lon
+        self.lon_0 = _lon_0
+        self.lat_0 = _lat_0
+        self.color_map_pos = _color_map_pos
+        self.color_map_cax = _color_map_cax
+        self.color_map_shrink = _color_map_shrink
+        self.color_map_orientation = _color_map_orientation
+        self.color_map_thickness = _color_map_thickness
+        self.color_map_label = _color_map_label
+        self.background = _background
+        self.land_color = _land_color
+        self.ocean_color = _ocean_color
+
+        if _n_quantized != 0:
+            self.color_map = plt.cm.get_cmap(_color_map, _n_quantized)
+        else:
+            self.color_map = plt.cm.get_cmap(_color_map)
 
     def draw(self):
 
+        mpl.rcParams.update({'font.size': 8})
+
+        if self.projection == 'moll':
+            step_tick_lat = 30.0
+            step_tick_lon = 60.0
+
+        else:
+            step_tick_lat = int(
+                (self.urcrnrlat - self.llcrnrlat) / self.n_tick_lat)
+            step_tick_lon = int(
+                (self.urcrnrlon - self.llcrnrlon) / self.n_tick_lon)
+
         self.basemap = Basemap(projection=self.projection, llcrnrlat=self.llcrnrlat, urcrnrlat=self.urcrnrlat,
-                               llcrnrlon=self.llcrnrlon, urcrnrlon=self.urcrnrlon, lat_ts=self.lat_ts, resolution=self.resolution)
+                               llcrnrlon=self.llcrnrlon, urcrnrlon=self.urcrnrlon, lat_ts=self.lat_ts, lon_0=self.lon_0, lat_0=self.lat_0, resolution=self.resolution)
 
         if self.draw_coast_lines:
             self.basemap.drawcoastlines()
@@ -36,23 +73,47 @@ class PMap(object):
         if self.draw_states:
             self.basemap.drawstates()
 
-        parallels = np.arange(-90., 91., 5.)
-        meridians = np.arange(-180., 181., 10.)
+        parallels = np.arange(-90., 91., step_tick_lat)
+        meridians = np.arange(-180., 181., step_tick_lon)
 
         self.fig_parallels = self.basemap.drawparallels(
             parallels, labels=[True, False, False, True])
         self.fig_meridians = self.basemap.drawmeridians(
             meridians, labels=[True, False, False, True])
 
-        self.basemap.drawmapboundary(fill_color='white')
+        x, y = self.basemap(self.data.lons, self.data.lats)
 
-        x, y = self.basemap(self.data.lons , self.data.lats)
+        sc = plt.scatter(x, y, c=self.data.temp, vmin=self.data.lower,
+                         vmax=self.data.upper, cmap=self.color_map, s=20, edgecolors='none')
 
-        l, u, temperature = self.data.get_noise_values()
+        if self.color_map_cax:
 
-        sc = plt.scatter(x, y, c=temperature, vmin=l,
-                         vmax=u, cmap=self.color_map, s=20, edgecolors='none')
+            if self.color_map_pos == 'bottom' or self.color_map_pos == 'top':
+                orientation = 'horizontal'
+                pad = '10%'
+            else:
+                orientation = 'vertical'
+                pad = '30%'
 
-        cbar = plt.colorbar(sc, shrink=.5)
-        cbar.set_label('value')
+            divider = make_axes_locatable(plt.gca())
+            cax = divider.append_axes(
+                position=self.color_map_pos, size=self.color_map_thickness, pad=pad)
+
+            cbar = plt.colorbar(sc, cax=cax, orientation=orientation)
+
+        else:
+            cbar = plt.colorbar(sc, shrink=self.color_map_shrink,
+                                orientation=self.color_map_orientation, pad=0.1)
+
+        cbar.set_label(self.color_map_label)
+
+        if self.background =='bluemarble':
+            self.basemap.bluemarble()
+        elif self.background =='shadedrelief':
+            self.basemap.shadedrelief()
+        elif self.background =='etopo':
+            self.basemap.etopo()
+        else:
+            self.basemap.drawlsmask(land_color=self.land_color,ocean_color=self.ocean_color,lakes=True)
+
         plt.show()
